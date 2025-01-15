75367d4aa5e5d52183560365553527f70d45adc2
AndrewKutovy <andrewkutovy24@gmail.com>
2025-01-15 16:12:53 +0200

    complete Task2

--- src/components/UsersComponent/UserComponent/UserComponent.tsx
+++ src/components/UsersComponent/UserComponent/UserComponent.tsx
@@ -1,6 +1,6 @@
 import {IUser} from "../../../models/UserInterface.ts";
 import {FC} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     user: IUser
@@ -14,7 +14,7 @@ const UserComponent:FC<Props> = ({user}) => {
 
     return (
         <div>
-            <Link to={'cart'} state={user}>{user.firstName}-{user.lastName}</Link>
+            {user.firstName}-{user.lastName}
             <button onClick={handleOnClick} >go to cart</button>
         </div>
     );
